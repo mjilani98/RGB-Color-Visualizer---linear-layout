a50cbddbd7b803a982f5011339115e2bc09f2ce9
mjilani98 <mjilani@emich.edu>
2025-02-13 11:13:47 -0500

    Created a ColorRGB class (Model)

---
+++ app/src/main/java/com/example/hw2q3/ColorRGB.java
@@ -0,0 +1,40 @@
+package com.example.hw2q3;
+
+public class ColorRGB {
+
+    private int red;
+
+    private int green;
+
+    private int blue;
+
+    public void setRed(int red)
+    {
+        this.red = red;
+    }
+
+    public void setGreen(int green)
+    {
+        this.green=green;
+    }
+
+    public void setBlue(int blue)
+    {
+        this.blue = blue;
+    }
+
+    public int getRed()
+    {
+        return red;
+    }
+
+    public int getGreen()
+    {
+        return green;
+    }
+
+    public int getBlue()
+    {
+        return blue;
+    }
+}
